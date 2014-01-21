b94f21e0580246ab6148de7392a79cbcc77bf930
Diego Lapiduz <diego.lapiduz@cfpb.gov>
2014-01-21 16:19:07 -0500

    Use the proper exit code

--- test/test_artifacts.sh
+++ test/test_artifacts.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
 project_name=${PWD##*/}
+ret=0
 
 if ! grep $project_name README.md 1>/dev/null; then
     echo "$project_name was not found in README.md"
+    ret=1
 fi
 
 required_files=('CONTRIBUTING.md' 'COPYING.txt' 'README.md' 'TERMS.md')
@@ -10,8 +13,8 @@ required_files=('CONTRIBUTING.md' 'COPYING.txt' 'README.md' 'TERMS.md')
 for file in "${required_files[@]}"; do
     if [ ! -f $file ]; then
         echo "Required File: $file not found"
+        ret=1
     fi
 done
 
-
-
+exit $ret
